bf76f7189e049dc40b1903edd286b000f5709ecc
Lee0zoo <122675741+Lee0zoo@users.noreply.github.com>
2024-05-06 13:44:47 +0900

    Update

--- model/Student.java
+++ model/Student.java
@@ -3,10 +3,12 @@ package camp.model;
 public class Student {
     private String studentId;
     private String studentName;
+    private String studentFeeling;
 
-    public Student(String seq, String studentName) {
+    public Student(String seq, String studentName, String studentFeeling) {
         this.studentId = seq;
         this.studentName = studentName;
+        this.studentFeeling  = studentFeeling;
     }
 
     // Getter
@@ -17,5 +19,7 @@ public class Student {
     public String getStudentName() {
         return studentName;
     }
+
+    public String getStudentFeeling() { return studentFeeling; }
 
 }
